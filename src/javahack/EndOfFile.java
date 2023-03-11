28e9b31b4c3181ba42b94036b6c4e0a7a579ffcb
Joshua David Sta Rita <jdcstarita@gmail.com>
2023-03-11 15:27:22 +0800

    datatype size and scanner

---
+++ src/javahack/EndOfFile.java
@@ -0,0 +1,19 @@
+package javahack;
+
+import java.io.*;
+import java.util.*;
+
+public class EndOfFile {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int line = 1;
+        while(scan.hasNext()){
+            String s = scan.nextLine();
+            System.out.println(line + " "+s);
+            line++;
+        }
+        scan.close();
+    }
+}
